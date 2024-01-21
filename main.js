6e89a0a6a6377bc01571aaaa8f7c08a4ddb13eb9
Oikapi <seryy.gordeev.78@mail.ru>
2024-01-22 02:09:31 +0700

    add css and seach-radio

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 
 
 let formInput = document.querySelector(".buttonSearch")
-let submButton = document.querySelector("#submitButton")
-let inputValue = document.querySelector("#inputText").value
-let arrCeil = document.querySelectorAll(".tableRow")
-let resultNode = document.createElement("div")
-
+let tableRows = document.querySelectorAll(".tableRow")
+let resultNode = document.createElement("p")
+let columnToSearch = document.getElementsByName("searchColumn")
+let searchForm = document.querySelector("#searchForm")
+let results = []
 resultNode.className = "resultDiv"
-submButton.addEventListener("click",() => {
-    inputValue = document.querySelector("#inputText").value
-    arrCeil.forEach((el) => el.children[1].style.color = "black")
+
+const getRadioValue = () =>{
+    for(let i = 0; i < columnToSearch.length; i++){
+        if(columnToSearch[i].checked){
+            console.log(columnToSearch[i].value)
+            return columnToSearch[i].value
+
+        }
+    }
+}
+const seachInTable = (columnIndex) => {
+    let inputValue = document.querySelector("#inputText").value
+
+    results.forEach((el) => el.style.color = "black")
+    results =[]
     if(formInput.lastChild == resultNode){
         formInput.removeChild(resultNode)
     }
     
     let count = 0;
-    for(let i = 0; i < arrCeil.length;i++){
-        
-        if(arrCeil[i].children[1].textContent.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()) ){
-            arrCeil[i].children[1].style.color = "red"
+    for(let i = 0; i < tableRows.length;i++){
+        console.log(i)
+        if(tableRows[i].children[columnIndex].textContent.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()) ){
+            tableRows[i].children[columnIndex].style.color = "red"
+            results.push(tableRows[i].children[columnIndex])
             count+=1
         }
     }
@@ -29,4 +42,8 @@ submButton.addEventListener("click",() => {
         resultNode.innerText = `Ничего не найдено`  
     }
     formInput.append(resultNode)
-})
+}
+
+
+searchForm.addEventListener("submit",(e) =>{e.preventDefault()
+    seachInTable(getRadioValue())})
